fbc278d4e4d7a6a462cd3c68930ff689d111456e
Hanzala Khan <pythondeveloper360@gmail.com>
2025-02-22 07:45:00 +0500

    added some functionalities

--- main.js
+++ main.js
@@ -1,13 +1,91 @@
-const prompt = require('prompt-sync')()
+const prompt = require("prompt-sync")();
 
-helpMessage =  `
-$add todo # adds todo with title todo
-$show todo # details about todo 
-$done todo # flag todo as todo as done
-$
+var flag = true;
 
-`
-do{
-    console.log()
-    input = prompt("")   
-}while(true)
+helpMessage = `
+$add todo         # adds todo with title todo,
+$add todo date    # adds todo with deadline, this will prioritize todo
+$show             # prints all todos
+$show todo        # details about todo 
+$done todo        # flag todo as todo as done
+$quit             # to quit application
+
+`;
+var todos = [];
+function showAllTodos() {
+  console.log(todos);
+}
+
+function showTodo(todo) {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].todo == todo) {
+      console.log(todos[i]);
+      return;
+    }
+  }
+  console.log(`No todo found with this title`);
+}
+function addTodo(todo, date) {
+  if (date) {
+    todos.push({ todo, date, status: "Pending" });
+  } else {
+    todos.push({ todo, status: "Pending" });
+  }
+}
+function setDone(todo) {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].todo == todo) {
+      todos[i].status = "Done";
+      return;
+    }
+  }
+  console.log("No todo found with this title");
+}
+
+do {
+  // console.log(helpMessage)
+  input = prompt("$");
+  if (input) {
+    tokens = input.split(" ");
+    switch (tokens[0]) {
+      case "add": {
+        let todo = tokens[1];
+        let date = tokens[2];
+        addTodo(todo, date);
+        break;
+      }
+
+      case "show": {
+        if (tokens[1]) {
+          let todo = tokens[1];
+          showTodo(todo);
+        } else {
+          showAllTodos();
+        }
+        break;
+      }
+      case "quit": {
+        flag = false;
+        break;
+      }
+      case "done": {
+        let todo = tokens[1];
+        if (todo) {
+          setDone(todo.trim());
+        } else {
+          console.log("Please specify todo name");
+        }
+        break;
+      }
+      case "help": {
+        console.log(helpMessage);
+        break;
+      }
+      default: {
+        console.log("Invalid Syntax");
+        console.log("type help for help message");
+        // console.log(helpMessage);
+      }
+    }
+  }
+} while (flag);
